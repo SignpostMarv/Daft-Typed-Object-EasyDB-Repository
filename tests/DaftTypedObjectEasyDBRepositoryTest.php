5541df28357e5b08fb97e7f95a86f88a44db96fb
SignpostMarv <me@signpostmarv.name>
2019-08-28 21:43:01 +0100

    fleshing out testing

--- tests/DaftTypedObjectEasyDBRepositoryTest.php
+++ tests/DaftTypedObjectEasyDBRepositoryTest.php
@@ -9,6 +9,14 @@ namespace SignpostMarv\DaftTypedObject;
 use ParagonIE\EasyDB\EasyDB;
 use ParagonIE\EasyDB\Factory;
 
+/**
+* @template S as array<string, scalar|null>
+* @template S2 as array<string, scalar|null>
+* @template T as array<string, scalar|array|object|null>
+* @template T1 as DaftTypedObjectForRepository
+*
+* @template-extends DaftTypedObjectRepositoryTest<S, S2, T, T1>
+*/
 class DaftTypedObjectEasyDBRepositoryTest extends DaftTypedObjectRepositoryTest
 {
 	public function dataProviderAppendTypedObject() : array
@@ -64,5 +72,68 @@ class DaftTypedObjectEasyDBRepositoryTest extends DaftTypedObjectRepositoryTest
 		}
 
 		return $out;
+	}
+
+	/**
+	* @template K as key-of<S>
+	*
+	* @dataProvider dataProviderAppendTypedObject
+	*
+	* @param class-string<AppendableTypedObjectRepository> $repo_type
+	* @param array{type:class-string<T1>} $repo_args
+	* @param array<int, S> $append_these
+	* @param array<int, S2> $expect_these
+	*/
+	public function testAppendTypedObject(
+		string $repo_type,
+		array $repo_args,
+		array $append_these,
+		array $expect_these
+	) : void {
+		parent::testAppendTypedObject(
+			$repo_type,
+			$repo_args,
+			$append_these,
+			$expect_these
+		);
+
+		if (Fixtures\MutableForRepository::class === $repo_args['type']) {
+			$repo = new $repo_type(
+				$repo_args
+			);
+
+			$object_type = $repo_args['type'];
+
+			/**
+			* @var array<int, T1>
+			*/
+			$testing = [];
+
+			foreach ($append_these as $i => $data) {
+				/**
+				* @var T1
+				*/
+				$object = $object_type::__fromArray($data);
+
+				$testing[$i] = $repo->AppendTypedObject($object);
+
+				$data['name'] = strrev($testing[$i]->name);
+				$data['id'] = $testing[$i]->id;
+
+				$repo->ForgetTypedObject($object->ObtainId());
+
+				$replacing_with = $object_type::__fromArray($data);
+
+				$fresh = $repo->RecallTypedObject($replacing_with->ObtainId());
+
+				$this->assertSame($object->name, $fresh->name);
+
+				$repo->UpdateTypedObject($replacing_with);
+
+				$fresh = $repo->RecallTypedObject($replacing_with->ObtainId());
+
+				$this->assertNotSame($object->name, $fresh->name);
+			}
+		}
 	}
 }
